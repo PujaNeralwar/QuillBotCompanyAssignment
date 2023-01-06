550d27c2b3a77e7aad3b739929894b0e8e09c4aa
PujaNeralwar <102819825+PujaNeralwar@users.noreply.github.com>
2023-01-06 23:00:39 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
 
+import './App.css';
+import FirstPage from './components/FirstPage';
+import Navbar from './components/Navbar';
+import SecondPage from './components/SecondPage';
+import ThirdPage from './components/ThirdPage';
+function App() {
+  return (
+    <>
+    <Navbar />
+    <FirstPage />
+    <SecondPage />
+    <ThirdPage />
+    </>
+  );
+}
+
+export default App;
